ba22995937c9dcdf53dc7df7715b8a49802a9f46
Eugene Foo <eugenefoo.cpf@gmail.com>
2023-04-08 00:25:52 +0800

    fix animation causing page resize

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,25 +9,28 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex flex-wrap ${styles.paddingY} justify-center sm:px-24 py-6 px-4`}
+      className={`flex flex-wrap ${styles.paddingY} justify-center py-6`}
     >
-      <img src={hero} alt="checkMate" className="gradient-mask-b-50 z-[5]" />
+      <img
+        src={hero}
+        alt="checkMate"
+        className="gradient-mask-b-50 z-[5] sm:px-24 px-4"
+      />
 
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className={`sm:px-16 xs:px-8 self-center`}
+        viewport={{ once: false, amount: "0.25" }}
+        className={`self-center w-full sm:w-auto overflow-x-hidden sm:px-24 px-8`}
       >
-        <motion.div variants={fadeIn("left", "tween", 0.2, 1)}>
+        <motion.div
+          variants={fadeIn("left", "tween", 0.2, 1)}
+          className="items-center"
+        >
           <div className="flex flex-col items-start sm:text-6xl text-5xl font-poppins font-semibold text-checkPurple">
-            <p className="leading-normal">
-              You flag,
-            </p>
-            <p>
-              we check
-            </p>
+            <p className="leading-normal">You flag,</p>
+            <p>we check</p>
           </div>
 
           <p className={`${styles.paragraph} text-checkPurple max-w-xs mt-5`}>
