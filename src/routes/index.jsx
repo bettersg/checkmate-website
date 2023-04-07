58c4d44e708f9dafa403c4a31434a7e945b71299
Eugene Foo <eugenefoo.cpf@gmail.com>
2023-04-07 23:56:00 +0800

    removed extra brackets

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,5 @@
 import { Setup, Work, CTA, Hero } from "../components";
 
-
 export default function Index() {
     return (
   <div className="bg-primary w-full">
@@ -12,20 +11,21 @@ export default function Index() {
     
     <div className="bg-checkPurple sm:px-16 px-6 flex justify-center items-center">
       <div className="xl:max-w-[1280px] w-full">
-        {<Setup />}
+          <Setup />
       </div>
     </div>
 
     <div className="bg-checkWhite sm:px-16 px-6 flex justify-center items-center">
       <div className="xl:max-w-[1280px] w-full">
-        {<Work />}
+          <Work />
       </div>
     </div>
 
     <div className="bg-checkWhite sm:px-16 px-6 flex justify-center items-center">
       <div className="xl:max-w-[1280px] w-full">
-        {<CTA />}
+          <CTA />
       </div>
     </div>
   </div>
-)}
+  );
+}
