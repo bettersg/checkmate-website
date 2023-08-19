b1951023ff3005d105d680df100538138355b99c
Eugene Foo <eugenefoo.cpf@gmail.com>
2023-08-19 10:56:20 +0800

    fix breakpoint method

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,7 +1,9 @@
 import { theme } from "tailwind.config.cjs";
 
 const getCurrentBreakpoint = () => {
-    const result = Object.entries(theme.screens).find(([key, val]) => parseInt(val.slice(0, -2)) >= window.innerWidth);
+    const result = Object.entries(theme.screens).reverse().find(([key, val]) => {
+        return window.innerWidth >= parseInt(val.slice(0, -2))
+    });
     return result ? result[0] : "xl"
 }
 
