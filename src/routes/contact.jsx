148cc32333cb032df6544f0bccd506a2fdfcdcd7
vincentteyssier <v.teyssier@gmail.com>
2023-08-25 18:22:15 +0800

    Adding google form

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -14,7 +14,9 @@ const Contact = () => {
       <div className="flex flex-col gap-y-6 text-xl text-checkBlack xl:max-w-[1280px] w-full p-12 items-start mx-auto">
         <p className="">We value your feedback and welcome opportunities for collaboration. Don't hesitate to reach out to us - we're excited to hear from you! You can also write to us at checkmate@better.sg.</p>
       </div>
-
+      <div className="w-full mx-auto max-w-[1280px] flex justify-center h-[100vh]">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScd3pL2uZ4CZOJpEj7QDE-zj35i4QrIKr0KUukSQjz5-v538A/viewform?embedded=true" width="640" height="500" >Loading…</iframe>
+      </div>
     </div>
   );
 };
