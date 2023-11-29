175155299e079bb118e6af823ac71bede829e2ff
Yuki Neo <yukineowq@Yukis-MacBook-Pro.local>
2023-11-29 23:15:35 +0800

    merged with webv2 commits

--- src/components/OurImpact.jsx
+++ src/components/OurImpact.jsx
@@ -36,15 +36,15 @@ const OurImpact = () => {
     });
 
     const Impact = () => (
-        <div className="w-full md:w-3/4 bg-checkWhite shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
+        <div style={{ width: "100%" }} className="w-full md:w-3/4 bg-checkWhite shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
             <h2 className="font-bold ss:text-[48px] text-[36px] text-checkPrimary600">Our Impact</h2>
             <p className="text-lg md:pr-8 text-gray-700">We've just started for now, but we hope to make a real dent on the problem of misinformation and scams, both in Singapore and around the world.</p>
         </div>
-    )
+    )   
 
     const MessagesChecked = () => (
-        <div className="w-full bg-checkWhite shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
-            <h2 className="font-bold ss:text-[48px] text-[36px] text-center">
+        <div style={{ minWidth: "100%", flexShrink: 0}} className="w-full bg-checkWhite shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
+            <h2 className="font-bold ss:text-[48px] text-[36px]">
                 <span className="from-checkPrimary600 to-checkSecondaryYellow500 bg-gradient-to-b bg-clip-text text-transparent">320+&nbsp;</span>
                 Message <br/>Checked
             </h2>
@@ -52,16 +52,16 @@ const OurImpact = () => {
     )
 
     const MessagesVerified = () => (
-        <div className="w-full bg-checkPrimary600 shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
-            <h2 className="font-bold ss:text-[48px] text-[36px] text-center text-white">
+        <div style={{ width: "100%" }} className="w-full bg-checkPrimary600 shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
+            <h2 className="font-bold ss:text-[48px] text-[36px] text-white">
                 50+ Messages <br/>Verified Weekly
             </h2>
         </div>
     )
 
     const ActiveCheckers = () => (
-        <div className="w-full bg-checkSecondaryYellow500 shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
-            <h2 className="font-bold ss:text-[48px] text-[36px] text-center text-white">
+        <div style={{ width: "100%" }} className="w-full bg-checkSecondaryYellow500 shadow-2xl px-12 py-8 rounded-carousel flex flex-col gap-y-4">
+            <h2 className="font-bold ss:text-[48px] text-[36px] text-white">
                 &gt;30 Active <br/>Checkers
             </h2>
         </div>
