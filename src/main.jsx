caa964f87967fe9186246d3e70add566192a7520
Brennan <tanjialebrennan@gmail.com>
2024-02-14 11:25:41 +0800

    feat: add i18n related pacakges

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import ErrorPage from "./routes/404";
-import Index from "./routes/index"
+import Index from "./routes/index";
 
-import './index.css';
-import DocumentMeta from 'react-document-meta';
+import "./index.css";
+import DocumentMeta from "react-document-meta";
 
-import {
-  BrowserRouter
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import TagManager from 'react-gtm-module'
+import TagManager from "react-gtm-module";
 
 const tagManagerArgs = {
-  gtmId: 'GTM-WDQ46XW'
-}
-
-TagManager.initialize(tagManagerArgs)
-
-const meta = {
-  
+  gtmId: "GTM-WDQ46XW",
 };
 
+TagManager.initialize(tagManagerArgs);
 
+const meta = {};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./i18n";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DocumentMeta {...meta}>
       <BrowserRouter>
