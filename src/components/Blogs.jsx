07cea95999a1a9c2e2f1778797ceadc4a6648e11
Brennan <tanjialebrennan@gmail.com>
2024-02-22 22:20:13 +0800

    feat: add Blogs page with data-fetching

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const demoEndpoint = import.meta.env.VITE_FIREBASE_NOTION_ENDPOINT;
 
 const Blogs = () => {
-  
-  return <div>Blogs</div>;
+  const [notionData, setNotionData] = useState("");
+
+  const handleNotionDataRetrievalClick = async () => {
+    const data = await axios.get(demoEndpoint);
+    setNotionData(JSON.stringify(data));
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <button
+        className="border border-black rounded p-3"
+        onClick={handleNotionDataRetrievalClick}
+      >
+        GET NOTION API DATA
+      </button>
+
+      <p>{notionData}</p>
+    </div>
+  );
 };
 
 export default Blogs;
