328f980cfae9eb4ce78bb2e44f4a387ed55567cb
Brennan <tanjialebrennan@gmail.com>
2024-03-09 21:27:54 +0800

    feat: style preview page

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -36,34 +36,75 @@ const convertJSONToComponents = (entries) => {
 
 const Blogs = () => {
   const [notionData, setNotionData] = useState("");
-  const [notionDataComponents, setNotionDataComponents] = useState([]);
+  const [blogPosts, setBlogPosts] = useState([]);
 
   const handleNotionDataRetrievalClick = async () => {
     const data = (await axios.get(demoEndpoint)).data;
 
-    // setNotionData(JSON.stringify(data));
     const components = convertJSONToComponents(data);
-    setNotionDataComponents(components);
+    setBlogPosts(components);
   };
 
   const SummaryPage = () => {
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col divide-y text-sm sm:text-lg">
         {/* Nav -- Refactor to Layout component */}
 
         {/* Page Data  */}
-        {notionDataComponents.map((component, index) => {
+        {blogPosts.map((component, index) => {
           return (
-            <div key={index}>
-              <h1>{component.summaryData.summary}</h1>
-              <p>{component.summaryData.duration}</p>
-              <img src={component.summaryData.previewImageURL} alt="preview" />
-              <p>{component.summaryData.Tags.join(", ")}</p>
+            <div
+              key={index}
+              className="flex flex-col gap-y-3 py-5 sm:py-10 container px-4 sm:px-0 justify-center"
+            >
+              <div className="flex gap-2">
+                {/* Author Name */}
+                <p>Author Name</p>
+                {/* Date */}
+                <p>
+                  {new Date("05-02-2024").toLocaleDateString("en-GB", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center">
+                {/* Image */}
+                <img
+                  src={component.summaryData.previewImageURL}
+                  alt="preview"
+                  className="w-28 h-28 object-cover rounded-md mr-4"
+                />
+                {/* Title, Summary */}
+                <div className="flex flex-col">
+                  <h2 className="capitalize text-lg font-bold">
+                    long title goes here
+                  </h2>
+                  <p className="hidden sm:inline-block">
+                    {/* {component.summaryData.summary.substring(0, 50)}
+                    {component.summaryData.summary.length > 50 ? "..." : ""} */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehe
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-4 items-center">
+                <div className="flex gap-x-1">
+                  {component.summaryData.Tags.map((tag) => (
+                    <span className="inline-block border rounded-full bg-slate-300 p-1 text-sm">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <p>{component.summaryData.duration} min read</p>
+              </div>
             </div>
           );
         })}
-
-        {/* Pagination */}
       </div>
     );
   };
@@ -77,6 +118,7 @@ const Blogs = () => {
         GET NOTION API DATA
       </button>
       <hr />
+
       <SummaryPage />
     </div>
   );
